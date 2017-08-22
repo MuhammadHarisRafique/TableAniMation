8476aae10fe38a4afb641b4bd61b6230add4813d
anas <anas.ejaz1991@gmail.com>
2017-08-22 17:38:06 +0500

    Initial Commit

---
+++ StepsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StepsDemo
+//
+//  Created by Higher Visibility on 22/08/2017.
+//  Copyright © 2017 HigherVisibility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
